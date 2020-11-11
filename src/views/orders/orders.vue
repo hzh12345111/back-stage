a8e50afb4672c1ebdf05dd75e5185ca3f8f48231
hzh12345111 <1667079692@qq.com>
2020-11-11 15:14:42 +0800

    修改orders

--- src/views/orders/orders.vue
+++ src/views/orders/orders.vue
@@ -1,10 +1,20 @@
 <template>
-  <div>orders</div>
+  <div class="orders">
+      <!-- 面包屑 -->
+    <bread-crumb :breadCrumbObj="breadCrumbObj"></bread-crumb>
+  </div>
 </template>
 
 <script>
 export default {
-
+data() {
+  return {
+     breadCrumbObj: {
+        second: "订单管理",
+        third: "订单列表",
+      },
+  }
+},
 }
 </script>
 
