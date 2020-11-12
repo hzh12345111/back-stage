66dd44b6010f506c71cdb2bcf24b1fab16f3c556
hzh12345111 <1667079692@qq.com>
2020-11-12 14:49:17 +0800

    完成订单管理数据显示

--- src/views/goods/goods.vue
+++ src/views/goods/goods.vue
@@ -1,13 +1,119 @@
 <template>
-  <div>goods</div>
+  <div class="goods">
+    <!-- 面包屑 -->
+    <bread-crumb :breadCrumbObj="breadCrumbObj"></bread-crumb>
+    <!-- 查询框 -->
+    <div class="check">
+      <el-input placeholder="请输入内容" v-model="query" class="ipt" @keyup.enter.native="searchUsers()">
+        <el-button slot="append" @click="searchUsers()" icon="el-icon-search"></el-button>
+      </el-input>
+      <el-button type="primary"  class="adduser" >添加商品</el-button>
+    </div>
+    <!-- 表格 -->
+    <template>
+       <el-table :data="obj.goods" border="" class="biao">
+        <el-table-column type="index" width="55" label="#"></el-table-column>
+        <el-table-column label="商品名称" prop="goods_name" width="754"></el-table-column>
+        <el-table-column label="商品价格(元)" prop="goods_price" width="100"></el-table-column>
+        <el-table-column label="商品重量" prop="goods_weight" width="100"></el-table-column>
+        <el-table-column label="创建日期" prop="upd_time" width="120">
+          <template slot-scope='slot'>
+            {{slot.row.upd_time|handelDate}}
+          </template>
+        </el-table-column>
+        <el-table-column label="操作" width="120">
+          <template slot-scope="scope">
+            <el-button type="primary" icon="el-icon-edit" circle size='small'></el-button>
+            <el-button type="danger" icon="el-icon-delete" circle size='small'></el-button>
+          </template>  
+        </el-table-column>
+      </el-table>
+    </template>
+    <!-- 分页 -->
+    <el-pagination
+      style="margin-top: 15px"
+      :current-page="params.pagenum"
+      :page-sizes="[10, 20, 30, 40]"
+      :page-size="params.pagesize"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="obj.total"
+      @size-change='handelSizeChange'
+      @current-change='handelCurrentChange'
+    ></el-pagination>
+  </div>
 </template>
 
 <script>
+import dayjs from 'dayjs'
 export default {
-
-}
+  data() {
+    return {
+      breadCrumbObj: {
+        second: "商品管理",
+        third: "商品列表",
+      },
+      params: {
+        query: "",
+        pagenum: 1,
+        pagesize: 10,
+      },
+      query:'',
+      obj:[]
+    };
+  },
+  methods: {
+    async gitGoodsList(){
+      const res=await this.$axios('goods',{},"GET",this.params)
+      this.obj=res.data;
+    },
+    handelSizeChange(val){ 
+      //每页条数发生变化 ，获取用户列表
+      setTimeout(async()=>{
+        this.params.pagesize=val;
+        const res = await this.$axios("goods", {}, "GET", {...this.params,pagesize:val});
+        this.obj = res.data;
+      },100)
+    },
+    async handelCurrentChange(val){
+      //页码跳转
+      this.params.pagenum=val;
+      console.log(val)
+      const res =await this.$axios("goods", {}, "GET", {...this.params,pagenum:val});
+      this.obj = res.data;
+      console.log(this.obj)
+    },
+    async searchUsers(){
+      //搜索框 查询
+      const res =await this.$axios("goods", {}, "GET", {...this.params,query:this.query});
+      this.params.query=this.query;
+      this.obj = res.data;
+    },
+  },
+  filters:{ //过滤时间
+    handelDate(val){
+       return dayjs(val).format("YYYY-MM-DD");
+    }
+  },
+  created() {
+    this.gitGoodsList()
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style lang="scss" scoped>
+.check {
+  margin-top: 15px;
+  .ipt {
+    margin-right: 10px;
+    margin-bottom: 20px;
+    width: 264px;
+  }
+}
+.biao{
+  height: 444px;
+  overflow-y:inherit ;
+}
+.el-table--scrollable-x .el-table__body-wrapper {
+    overflow-x: hidden;
+}
+</style>>
--- src/views/orders/orders.vue
+++ src/views/orders/orders.vue
@@ -1,23 +1,89 @@
 <template>
   <div class="orders">
-      <!-- 面包屑 -->
-    <bread-crumb :breadCrumbObj="breadCrumbObj"></bread-crumb>
+    <!-- 面包屑 -->
+     <bread-crumb :breadCrumbObj="breadCrumbObj"></bread-crumb>
+    <el-table :data="orderList.goods" style="width: 100%;margin-top:20px" border>
+      <el-table-column type="index" width="50" label="#"></el-table-column>
+      <el-table-column label="订单编号" prop="order_number"></el-table-column>
+      <el-table-column label="订单价格" prop="order_price"></el-table-column>
+      <el-table-column label="是否付款">
+        <template slot-scope="scope">
+          <el-button  :type="scope.row.order_pay==0?'danger':'primary'" plain size="small" >{{scope.row.order_pay | filOrdePay}}</el-button>
+          </template>
+        
+      </el-table-column>
+      <el-table-column label="是否发货" prop="is_send"></el-table-column>
+      <el-table-column label="下单时间" prop="update_time"></el-table-column>
+      <el-table-column label="操作">
+        <el-button icon="el-icon-edit" size="small" plain></el-button>
+      </el-table-column>
+    </el-table>
+    <!-- 分页 -->
+    <el-pagination
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="params.pagenum"
+      :page-sizes="[10, 20, 30, 40]"
+      :page-size="params.pagesize"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="orderList.total"
+    ></el-pagination>
+    
   </div>
 </template>
-
 <script>
 export default {
-data() {
-  return {
-     breadCrumbObj: {
+  data() {
+    return {
+      orderList: [],
+      params:{
+        pagenum: 1,
+        pagesize: 10
+      },
+      breadCrumbObj: {
         second: "订单管理",
         third: "订单列表",
       },
+    };
+  },
+  methods: {
+    //获取订单列表
+    async getOrderList() {
+      const res=await this.$axios('orders',{},'GET',this.params)
+      this.orderList=res.data
+    },
+    //显示条数
+    handleSizeChange(num){
+      this.params.pagesize=num;
+      this.getOrderList()
+    },
+    //页数
+    handleCurrentChange(num){
+      this.params.pagenum=num;
+      this.getOrderList()
+    }
+  },
+  created(){
+    this.getOrderList()
+  },
+  filters:{
+    filOrdePay(msg){
+      if(msg==0){
+          return "未付款"
+      }else if(msg==1){
+         return "已付款"
+      }else if(msg==2){
+         return "待定"
+      }
+    }
   }
-},
-}
+};
 </script>
-
-<style>
-
+<style lang="scss" scoped>
+.orders {
+  width: 100%;
+  padding: 15px;
+  box-sizing: border-box;
+  border-radius: 5px;
+}
 </style>
